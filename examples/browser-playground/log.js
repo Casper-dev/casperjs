02dabdded852f38fe73d06d94fe8599a46455d6a
v-trof <seva.trofimov2@gmail.com>
2018-07-06 13:49:31 +0300

    Now this is really GH pages only

--- examples/browser-playground/log.js
+++
@@ -1,6 +0,0 @@
-const log = (message, payload) => {
-  const entry = `[${new Date().toLocaleString()}] ${message}: ${payload}\n`
-  document.getElementById('log').value += entry
-}
-
-export default log
